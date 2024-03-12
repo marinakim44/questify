75de71ab8c2649d979bb4ba7f05a4b9fd2d3a6ed
Marina Kim <91118219+marinakim44@users.noreply.github.com>
2024-03-12 13:43:12 +0000

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,17 +18,17 @@ const loginUser = async (req, res) => {
     });
     const payload = ticket.getPayload();
 
-    if (
-      ![
-        "marina.kim.mk90@gmail.com",
-        "marinamuse4490@gmail.com",
-        "mkim@rcgroup.online",
-      ].includes(payload.email)
-    ) {
-      return res
-        .status(401)
-        .json({ message: "You are not authorized to view this page" });
-    }
+    // if (
+    //   ![
+    //     "marina.kim.mk90@gmail.com",
+    //     "marinamuse4490@gmail.com",
+    //     "mkim@rcgroup.online",
+    //   ].includes(payload.email)
+    // ) {
+    //   return res
+    //     .status(401)
+    //     .json({ message: "You are not authorized to view this page" });
+    // }
 
     let user = await User.findOneAndUpdate(
       { email: payload.email },
